12540e801a97a8f7104587361bdc431854b27599
b4ibmjava09@iiht.tech <b4ibmjava09@iiht.tech>
2019-08-22 09:00:44 +0530

    assessment 2nd question updated

---
+++ assessment1_21st_19/src/comm/example2/Main.java
@@ -0,0 +1,23 @@
+package comm.example2;
+import java.util.Scanner;
+public class Main {
+ static Scanner sc=new Scanner(System.in);
+ static boolean result;
+	public static void main(String[] args) {
+		
+		Main.input();
+		Main.output(result);	
+	}
+	public static void input() {
+		System.out.println("enter password: ");
+		String password=sc.next();
+		result=UserMainCode.checkPassword(password);
+		
+	}
+	public static void output(boolean result) {
+		if(result==true) {
+			System.out.println("valid password");
+		}
+		else System.out.println("invalid password");
+	}
+}
---
+++ assessment1_21st_19/src/comm/example2/UserMainCode.java
@@ -0,0 +1,10 @@
+package comm.example2;
+
+public class UserMainCode {
+
+	
+	public static boolean checkPassword(String password){
+		  String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+	      return (password.matches(pattern));
+	}
+}
